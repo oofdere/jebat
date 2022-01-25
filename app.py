fcdcfe91be815eb01f33e8ff3b657c721cd61bc7
Tibet Tornaci <oofdere@hey.com>
2022-01-24 23:57:38 -0800

    Squashed commit of the following:
    commit 25f10c0fded5b9e8696cac378f4bbb65caf40bc2
    Merge: 40e2bcf 455c11f
    Author: Tibet Tornaci <oofdere@hey.com>
    Date:   Mon Jan 24 23:48:14 2022 -0800
    
        Merge branch 'admin' into main
    
    commit 40e2bcf77d71936853cfe285a1717ae18386ccca
    Author: Tibet Tornaci <oofdere@hey.com>
    Date:   Mon Jan 24 22:13:37 2022 -0800
    
        Initial migration
    
    commit 8be2ed2b3735e8215e49a4c6a413c9bbcf5f7a31
    Merge: 78eabf3 8ace56f
    Author: Tibet Tornaci <oofdere@hey.com>
    Date:   Mon Jan 24 22:06:00 2022 -0800
    
        Merge pull request #14 from oofdere/albums
    
        album functionality
    
    commit 8ace56fa93161db54cda8547fd1d1f3a3cdb83b6
    Merge: 4ee21d0 78eabf3
    Author: Tibet Tornaci <oofdere@hey.com>
    Date:   Mon Jan 24 21:31:50 2022 -0800
    
        Merge branch 'main' into albums
    
    commit 4ee21d02c1d2a50419efaf6205587e5af2f12b3e
    Author: Ernest J <thetankcoder@gmail.com>
    Date:   Mon Jan 24 21:30:25 2022 -0800
    
        resolved conflicts
    
    commit 78eabf39b60727103dbb9d2280d1b8d7f2fec2b2
    Merge: d0700ac 2ac5812
    Author: berlinfirewall <36656921+berlinfirewall@users.noreply.github.com>
    Date:   Mon Jan 24 21:26:49 2022 -0800
    
        Merge pull request #15 from oofdere/thumbnails
    
        Thumbnails
    
    commit f57beed1a06d3ef65b2a3402999e92096cd7b578
    Author: Tibet Tornaci <oofdere@hey.com>
    Date:   Mon Jan 24 21:20:54 2022 -0800
    
        remove hardcoded variables
    
    commit 2ac5812f7ac961cff2983650a91ff9336fb68d2c
    Author: berlinfirewall <gentooinstaller@protonmail.com>
    Date:   Mon Jan 24 21:17:32 2022 -0800
    
        fix dumb error jpeg
    
    commit 99c87907fbab49eb057f2db214aa6fb1f6f756b0
    Author: Ernest J <thetankcoder@gmail.com>
    Date:   Mon Jan 24 21:16:42 2022 -0800
    
        album functionality
    
    commit 84ccaa640ea55c155c972c21ebac738e71452a56
    Author: berlinfirewall <gentooinstaller@protonmail.com>
    Date:   Mon Jan 24 21:06:39 2022 -0800
    
        Secure filename fix
    
    commit b330891e82b6fc77f3561b090e94a0ed1fe493dc
    Author: berlinfirewall <gentooinstaller@protonmail.com>
    Date:   Mon Jan 24 20:49:11 2022 -0800
    
        Added thumbnail creation

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from datetime import datetime
 import flask_login
 from sqlalchemy import desc
 
+import thumbnail
+
 from helpers import env
 
-from flask import Blueprint, Flask, flash, render_template, redirect, request, send_from_directory, url_for, flash
+from flask import Blueprint, Flask, flash, render_template, redirect, request, send_from_directory, url_for, flash, send_file
 from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 from werkzeug.utils import secure_filename
 
@@ -15,7 +17,6 @@ app = Flask(__name__)
 # by default it's /static
 app.config["SQLALCHEMY_DATABASE_URI"] = env("SQLALCHEMY_DATABASE_URI")
 app.config["SECRET_KEY"] = env("SECRET_KEY")
-
 login = LoginManager(app)
 login.login_view = "login"
 from models import *
@@ -92,15 +93,59 @@ def upload():
             db.session.commit()
             f.seek(0)
             f.save(os.path.join(app.root_path, env("IMAGE_DIR"), filename))
+            thumbnail.create(filename)
             return redirect(url_for('view', image_hash=image_hash))
     else:
         return render_template("upload.html", form=form)
 
 
+@app.route("/albums")
+def albums():
+    all_albums = Album.query.all()
+    print(all_albums)
+    return render_template("albums.html", albums=all_albums)
+
+
+@app.route("/create_album", methods=["GET", "POST"])
+@login_required
+def create_album():
+    form = AlbumForm()
+    if request.method == "POST":
+        print("post request to create album")
+        name_of_album = form.name.data
+        description_of_album = form.description.data
+        new_album = Album(name=name_of_album, description=description_of_album,
+                          user_id=current_user.id)
+        db.session.add(new_album)
+        db.session.commit()
+        return redirect(url_for('album', album_id=new_album.id))
+    return render_template("create_album.html", form=form)
+
+
+@app.route("/album/<int:album_id>", methods=["GET", "POST"])
+@login_required
+def album(album_id):
+    get_album = Album.query.filter_by(id=album_id).first()
+    album_images = get_album.images_in_album
+    return render_template("album.html", album=get_album, album_images=album_images)
+
+
+@app.route("/album/add/<int:img_id>", methods=["POST"])
+@login_required
+def add_to_album(img_id):
+    album_id = request.form['choose_album']
+    album_list = Album.query.filter_by(id=album_id).first()
+    get_image = Image.query.filter_by(id=img_id).first()
+    album_list.images_in_album.append(get_image)
+    db.session.commit()
+    return redirect(url_for('album', album_id=album_id))
+
+
 @app.route("/<image_hash>")
 def view(image_hash):
     image = Image.query.filter_by(hash=image_hash).first()
-    return render_template("detail_view.html", image=image)
+    all_albums = Album.query.all()
+    return render_template("detail_view.html", image=image, albums=all_albums)
 
 
 @app.route("/pool/<int:pool_id>")
@@ -108,13 +153,22 @@ def pool(pool_id):
     # show a collection of images
     pass
 
+  
+@app.route("/thumbnails/<filename>")
+def get_thumb(filename):
+    filepath = os.path.join(app.root_path, env("THUMB_DIR"), secure_filename(filename))
+    return send_file(filepath, mimetype='image/jpeg')
+
+  
 @app.route("/search")
 def search():
     # search images by tags
     pass
 
+  
 blueprint = Blueprint('images', __name__, static_url_path='/images', static_folder='images/')
 app.register_blueprint(blueprint)
+
 
 @app.route("/admin")
 @login_required
--- models.py
+++ models.py
@@ -7,6 +7,13 @@ from app import login
 
 db = SQLAlchemy()
 
+
+image_to_album = db.Table('image_to_album',
+                          db.Column('image_id', db.Integer, db.ForeignKey('image.id')),
+                          db.Column('album_id', db.Integer, db.ForeignKey('album.id'))
+                          )
+
+
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hash = db.Column(db.Text, unique=True, nullable=False)
@@ -14,7 +21,9 @@ class Image(db.Model):
     caption = db.Column(db.Text)
     exif = db.Column(db.Text)
     date = db.Column(db.DateTime, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    in_album = db.relationship("Album", secondary=image_to_album, backref='images_in_album')
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,10 +33,20 @@ class User(UserMixin, db.Model):
     can_upload = db.Column(db.Boolean)
     can_view = db.Column(db.Boolean)
     uploads = db.relationship("Image", backref="images", lazy=True)
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.Text)
+    description = db.Column(db.Text)
+
 
 @login.user_loader
 def load_user(id):
